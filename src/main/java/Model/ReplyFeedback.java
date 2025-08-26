2726a9bbc80e9ad108779bd11906cb5935573bc6
TriTN-ai <TriTNCE180273@fpt.edu.vn>
2025-08-26 16:13:10 +0700

    Update ReplyFeedback.java

--- src/main/java/Model/ReplyFeedback.java
+++ src/main/java/Model/ReplyFeedback.java
@@ -5,31 +5,77 @@
 package Model;
 
 import java.sql.Timestamp;
+
 /**
  *
  * @author TriTN
  */
 
-
 public class ReplyFeedback {
+
     private int replyID;
     private int feedbackID;
     private int userID;
     private String replyText;
     private Timestamp createdAt;
+    private boolean isDeleted;
+    private int roleID;
 
-    public int getReplyID() { return replyID; }
-    public void setReplyID(int replyID) { this.replyID = replyID; }
+    public int getRoleID() {
+        return roleID;
+    }
 
-    public int getFeedbackID() { return feedbackID; }
-    public void setFeedbackID(int feedbackID) { this.feedbackID = feedbackID; }
+    public void setRoleID(int roleID) {
+        this.roleID = roleID;
+    }
 
-    public int getUserID() { return userID; }
-    public void setUserID(int userID) { this.userID = userID; }
+    public int getReplyID() {
+        return replyID;
+    }
 
-    public String getReplyText() { return replyText; }
-    public void setReplyText(String replyText) { this.replyText = replyText; }
+    public void setReplyID(int replyID) {
+        this.replyID = replyID;
+    }
 
-    public Timestamp getCreatedAt() { return createdAt; }
-    public void setCreatedAt(Timestamp createdAt) { this.createdAt = createdAt; }
+    public int getFeedbackID() {
+        return feedbackID;
+    }
+
+    public void setFeedbackID(int feedbackID) {
+        this.feedbackID = feedbackID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getReplyText() {
+        return replyText;
+    }
+
+    public void setReplyText(String replyText) {
+        this.replyText = replyText;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public boolean getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    
 }
